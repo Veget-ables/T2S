2a400b362aa809913207121b0b84ea9d1bb49bc6
Veget-ables <makuhari109@gmail.com>
2024-07-05 18:30:48 +0900

    DatePickerとTimePickerを追加

--- app/src/main/java/com/tsuchinoko/t2s/ScheduleGenScreen.kt
+++ app/src/main/java/com/tsuchinoko/t2s/ScheduleGenScreen.kt
@@ -2,12 +2,13 @@ package com.tsuchinoko.t2s
 
 import android.content.Intent
 import android.provider.CalendarContract
-import android.widget.EditText
 import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.spring
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -16,14 +17,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -32,8 +36,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TimePicker
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.surfaceColorAtElevation
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -49,9 +55,9 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.tsuchinoko.t2s.ui.theme.T2STheme
-import java.time.LocalDate
 import java.time.LocalDateTime
 
 @Composable
@@ -218,9 +224,21 @@ private fun EditableEventContent(
                 ),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            EventDateTime(
+                date = event.simpleStartDate,
+                time = event.simpleStartTime
+            )
+            EventDateTime(
+                date = event.simpleEndDate,
+                time = event.simpleEndTime,
+                modifier = Modifier.padding(top = 8.dp)
+            )
+
             OutlinedTextField(
                 value = event.title,
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp),
                 onValueChange = {
                     val new = event.copy(title = it)
                     onEventChange(new)
@@ -247,6 +265,69 @@ private fun EditableEventContent(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun EventDateTime(date: String, time: String, modifier: Modifier = Modifier) {
+    var showDatePicker by remember { mutableStateOf(false) }
+    var showTimePicker by remember { mutableStateOf(false) }
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Absolute.SpaceBetween
+    ) {
+        Text(
+            text = date,
+            modifier = Modifier
+                .weight(1f, true)
+                .clickable {
+                    showDatePicker = true
+                }
+        )
+        Text(
+            text = time,
+            modifier = Modifier.clickable {
+                showTimePicker = true
+            }
+        )
+    }
+    if (showDatePicker) {
+        val datePickerState = rememberDatePickerState()
+        DatePickerDialog(
+            onDismissRequest = { showDatePicker = false },
+            confirmButton = { showDatePicker = false },
+            dismissButton = { showDatePicker = false }
+        ) {
+            DatePicker(state = datePickerState)
+        }
+    }
+    if (showTimePicker) {
+        val timePickerState = rememberTimePickerState()
+        TimePickerDialog(
+            onDismissRequest = { showTimePicker = false },
+        ) {
+            TimePicker(state = timePickerState)
+        }
+    }
+}
+
+@ExperimentalMaterial3Api
+@Composable
+private fun TimePickerDialog(
+    onDismissRequest: () -> Unit,
+    modifier: Modifier = Modifier,
+    properties: DialogProperties = DialogProperties(usePlatformDefaultWidth = false),
+    content: @Composable ColumnScope.() -> Unit
+) {
+    BasicAlertDialog(
+        onDismissRequest = onDismissRequest,
+        modifier = modifier.wrapContentHeight(),
+        properties = properties
+    ) {
+            Column(verticalArrangement = Arrangement.SpaceBetween) {
+                content()
+                // Buttons
+        }
+    }
+}
 
 @Composable
 private fun EventContent(event: ScheduleEvent, modifier: Modifier = Modifier) {
