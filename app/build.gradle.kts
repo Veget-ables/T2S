6c58f5ea110e35f5068f0b6451bdac92dbb983ab
Veget-ables <makuhari109@gmail.com>
2024-08-10 16:31:06 +0900

    不要な設定を削除

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,12 +31,6 @@ android {
     buildFeatures {
         buildConfig = true
     }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-            excludes += "/META-INF/DEPENDENCIES"
-        }
-    }
 }
 
 dependencies {
