7f945f4b14b95f4ebaae26d6ee6d2c329e27a94d
Veget-ables <makuhari109@gmail.com>
2024-08-14 17:53:15 +0900

    UiStateのコミットが漏れていた

---
+++ feature/build/spotless/spotlessKotlin/schedule/src/main/java/com/tsuchinoko/t2s/feature/schedule/ScheduleGenUiState.kt
@@ -0,0 +1,13 @@
+package com.tsuchinoko.t2s.feature.schedule
+
+internal data class ScheduleGenUiState(
+    val calendarUiState: CalendarUiState,
+    val generatedEventsUiState: GeneratedEventsUiState,
+) {
+    companion object {
+        val Empty = ScheduleGenUiState(
+            calendarUiState = CalendarUiState.Initial,
+            generatedEventsUiState = GeneratedEventsUiState.Empty,
+        )
+    }
+}
