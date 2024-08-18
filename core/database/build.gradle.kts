2c443a488435574a26b95beac17ab7ed68b9fc96
Veget-ables <makuhari109@gmail.com>
2024-08-18 09:13:13 +0900

    Roomの依存を追加

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -1,7 +1,13 @@
 plugins {
     alias(libs.plugins.t2s.android.library)
+    alias(libs.plugins.ksp)
 }
 
 android {
     namespace = "com.tsuchinoko.t2s.core.database"
+}
+
+dependencies {
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
 }
