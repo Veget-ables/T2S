49c2b91ab8f5252742588696be73d44392a46ee5
Veget-ables <makuhari109@gmail.com>
2024-08-25 17:48:07 +0900

    予定変換処理中にPlaceholderを表示するようにした

--- feature/schedule/src/main/java/com/tsuchinoko/t2s/feature/schedule/gen/GeneratedEvents.kt
+++ feature/schedule/src/main/java/com/tsuchinoko/t2s/feature/schedule/gen/GeneratedEvents.kt
@@ -1,13 +1,12 @@
 package com.tsuchinoko.t2s.feature.schedule.gen
 
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -16,8 +15,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.tsuchinoko.t2s.core.designsystem.component.placeholder
 import com.tsuchinoko.t2s.core.designsystem.them.T2STheme
 import com.tsuchinoko.t2s.core.model.ScheduleEvent
+import java.time.LocalDateTime
+import java.util.UUID
 
 internal sealed interface GeneratedEventsUiState {
     data object Loading : GeneratedEventsUiState
@@ -25,49 +27,65 @@ internal sealed interface GeneratedEventsUiState {
     data class Error(val message: String) : GeneratedEventsUiState
 }
 
-@Composable
-internal fun GeneratedEvents(
+internal fun LazyListScope.generatedEvents(
     uiState: GeneratedEventsUiState,
-    modifier: Modifier = Modifier,
     onEventChange: (ScheduleEvent) -> Unit = {},
 ) {
     when (uiState) {
         GeneratedEventsUiState.Loading -> {
-            CircularProgressIndicator()
+            items(items = skeletonEvents) { event ->
+                GeneratedEvent(
+                    event = event,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp)
+                        .placeholder(visible = true),
+                    onEventChange = onEventChange,
+                )
+                Spacer(Modifier.height(8.dp))
+            }
         }
 
         is GeneratedEventsUiState.Generated -> {
-            LazyColumn(
-                modifier = modifier,
-                contentPadding = PaddingValues(
-                    start = 8.dp,
-                    end = 8.dp,
-                    top = 16.dp,
-                    bottom = 112.dp,
-                ),
-            ) {
-                items(
-                    items = uiState.events,
-                    key = { it.id },
-                ) { event ->
-                    GeneratedEvent(
-                        event = event,
-                        modifier = Modifier.fillMaxWidth(),
-                        onEventChange = onEventChange,
-                    )
-                    Spacer(Modifier.height(8.dp))
-                }
+            items(
+                items = uiState.events,
+                key = { it.id },
+            ) { event ->
+                GeneratedEvent(
+                    event = event,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp)
+                        .placeholder(visible = false),
+                    onEventChange = onEventChange,
+                )
+                Spacer(Modifier.height(8.dp))
             }
         }
 
         is GeneratedEventsUiState.Error -> {
-            Text(
-                text = uiState.message,
-                textAlign = TextAlign.Start,
-                color = MaterialTheme.colorScheme.error,
-                modifier = Modifier.padding(16.dp),
-            )
+            item {
+                Text(
+                    text = uiState.message,
+                    textAlign = TextAlign.Start,
+                    color = MaterialTheme.colorScheme.error,
+                    modifier = Modifier.padding(16.dp),
+                )
+            }
         }
+    }
+}
+
+private val skeletonEvents: List<ScheduleEvent> = run {
+    val time = LocalDateTime.parse("2024-08-25T00:00")
+    (0..10).toList().map {
+        ScheduleEvent(
+            id = UUID.randomUUID(),
+            title = "",
+            memo = null,
+            start = time,
+            end = time,
+        )
     }
 }
 
@@ -76,7 +94,9 @@ internal fun GeneratedEvents(
 fun GeneratedEventsPreview_Generated() {
     T2STheme {
         Surface {
-            GeneratedEvents(uiState = GeneratedEventsUiState.Generated(fakeEvents))
+            LazyColumn {
+                generatedEvents(uiState = GeneratedEventsUiState.Generated(fakeEvents))
+            }
         }
     }
 }
@@ -86,7 +106,9 @@ fun GeneratedEventsPreview_Generated() {
 fun GeneratedEventsPreview_Error() {
     T2STheme {
         Surface {
-            GeneratedEvents(uiState = GeneratedEventsUiState.Error("予定の生成に失敗しました"))
+            LazyColumn {
+                generatedEvents(uiState = GeneratedEventsUiState.Error("予定の生成に失敗しました"))
+            }
         }
     }
 }
--- feature/schedule/src/main/java/com/tsuchinoko/t2s/feature/schedule/gen/ScheduleGenScreen.kt
+++ feature/schedule/src/main/java/com/tsuchinoko/t2s/feature/schedule/gen/ScheduleGenScreen.kt
@@ -5,20 +5,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.BottomAppBarDefaults
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -26,7 +22,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -147,40 +142,10 @@ private fun ScheduleGenScreen(
                     Spacer(Modifier.height(36.dp))
                 }
 
-                when (generatedEventsUiState) {
-                    GeneratedEventsUiState.Loading -> {
-                        item {
-                            CircularProgressIndicator()
-                        }
-                    }
-
-                    is GeneratedEventsUiState.Generated -> {
-                        items(
-                            items = generatedEventsUiState.events,
-                            key = { it.id },
-                        ) { event ->
-                            GeneratedEvent(
-                                event = event,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(horizontal = 20.dp),
-                                onEventChange = onEventChange,
-                            )
-                            Spacer(Modifier.height(8.dp))
-                        }
-                    }
-
-                    is GeneratedEventsUiState.Error -> {
-                        item {
-                            Text(
-                                text = generatedEventsUiState.message,
-                                textAlign = TextAlign.Start,
-                                color = MaterialTheme.colorScheme.error,
-                                modifier = Modifier.padding(16.dp),
-                            )
-                        }
-                    }
-                }
+                generatedEvents(
+                    uiState = generatedEventsUiState,
+                    onEventChange = onEventChange,
+                )
             }
         }
     }
