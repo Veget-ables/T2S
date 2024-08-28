acfc93b197b87cd7cbcadb614885b8998d646ba5
Veget-ables <makuhari109@gmail.com>
2024-08-28 22:32:52 +0900

    無駄な通信処理が発生していたのを修正

--- feature/schedule/src/main/java/com/tsuchinoko/t2s/feature/schedule/account/CalendarAccountViewModel.kt
+++ feature/schedule/src/main/java/com/tsuchinoko/t2s/feature/schedule/account/CalendarAccountViewModel.kt
@@ -15,6 +15,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
@@ -27,7 +28,9 @@ class CalendarAccountViewModel @Inject constructor(
     private val accountRepository: AccountRepository,
     private val calendarRepository: CalendarRepository,
 ) : ViewModel() {
-    private val account: Flow<Account?> = accountRepository.getAccount()
+    private val account: Flow<Account?> = accountRepository
+        .getAccount()
+        .distinctUntilChanged()
 
     val needAccount: StateFlow<Boolean> = account.map { it == null }
         .stateIn(
