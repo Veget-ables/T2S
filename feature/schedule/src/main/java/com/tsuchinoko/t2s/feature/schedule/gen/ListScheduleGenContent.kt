d6e35c471ba4abe86d01369b4ef5a3a7e10d6819
Veget-ables <makuhari109@gmail.com>
2024-08-31 17:16:16 +0900

    ListStyleのレイアウトを修正

--- feature/schedule/src/main/java/com/tsuchinoko/t2s/feature/schedule/gen/ListScheduleGenContent.kt
+++ feature/schedule/src/main/java/com/tsuchinoko/t2s/feature/schedule/gen/ListScheduleGenContent.kt
@@ -1,36 +1,26 @@
 package com.tsuchinoko.t2s.feature.schedule.gen
 
-import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.tsuchinoko.t2s.core.designsystem.component.placeholder
 import com.tsuchinoko.t2s.core.designsystem.them.T2STheme
 import com.tsuchinoko.t2s.core.model.ScheduleEvent
-import com.tsuchinoko.t2s.feature.schedule.R
 import java.time.LocalDateTime
 import java.util.UUID
 
@@ -41,61 +31,67 @@ internal fun ListScheduleGenContent(
     generatedEventsUiState: GeneratedEventsUiState,
     onEventChange: (ScheduleEvent) -> Unit = {},
 ) {
-    LazyColumn(
-        modifier = Modifier.padding(paddingValues),
+    Column(
+        modifier = Modifier
+            .padding(paddingValues)
+            .fillMaxSize(),
     ) {
-        item {
-            ScheduleInputCard(
-                text = prompt,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(start = 24.dp, top = 24.dp, end = 24.dp),
-            )
+        OutlinedTextField(
+            value = prompt,
+            onValueChange = {},
+            readOnly = true,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .weight(1f),
+        )
 
-            Spacer(Modifier.height(36.dp))
-        }
-
-        when (generatedEventsUiState) {
-            GeneratedEventsUiState.Loading -> {
-                items(items = skeletonEvents) { event ->
-                    GeneratedEvent(
-                        event = event,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(horizontal = 20.dp)
-                            .placeholder(visible = true),
-                        onEventChange = onEventChange,
-                    )
-                    Spacer(Modifier.height(8.dp))
+        LazyColumn(
+            modifier = Modifier
+                .weight(1f),
+        ) {
+            when (generatedEventsUiState) {
+                GeneratedEventsUiState.Loading -> {
+                    items(items = skeletonEvents) { event ->
+                        GeneratedEvent(
+                            event = event,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 20.dp)
+                                .placeholder(visible = true),
+                            onEventChange = onEventChange,
+                        )
+                        Spacer(Modifier.height(8.dp))
+                    }
                 }
-            }
 
-            is GeneratedEventsUiState.Generated -> {
-                val events = generatedEventsUiState.events
-                items(
-                    items = events,
-                    key = { it.id },
-                ) { event ->
-                    GeneratedEvent(
-                        event = event,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(horizontal = 20.dp)
-                            .placeholder(visible = false),
-                        onEventChange = onEventChange,
-                    )
-                    Spacer(Modifier.height(8.dp))
+                is GeneratedEventsUiState.Generated -> {
+                    val events = generatedEventsUiState.events
+                    items(
+                        items = events,
+                        key = { it.id },
+                    ) { event ->
+                        GeneratedEvent(
+                            event = event,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 20.dp)
+                                .placeholder(visible = false),
+                            onEventChange = onEventChange,
+                        )
+                        Spacer(Modifier.height(8.dp))
+                    }
                 }
-            }
 
-            is GeneratedEventsUiState.Error -> {
-                item {
-                    Text(
-                        text = generatedEventsUiState.message,
-                        textAlign = TextAlign.Start,
-                        color = MaterialTheme.colorScheme.error,
-                        modifier = Modifier.padding(16.dp),
-                    )
+                is GeneratedEventsUiState.Error -> {
+                    item {
+                        Text(
+                            text = generatedEventsUiState.message,
+                            textAlign = TextAlign.Start,
+                            color = MaterialTheme.colorScheme.error,
+                            modifier = Modifier.padding(16.dp),
+                        )
+                    }
                 }
             }
         }
@@ -112,54 +108,6 @@ internal val skeletonEvents: List<ScheduleEvent> = run {
             start = time,
             end = time,
         )
-    }
-}
-
-@Composable
-private fun ScheduleInputCard(
-    text: String,
-    modifier: Modifier = Modifier,
-) {
-    var isExpanded by rememberSaveable { mutableStateOf(false) }
-    Card(
-        modifier = modifier
-            .animateContentSize(),
-    ) {
-        OutlinedButton(
-            modifier = Modifier
-                .padding(
-                    start = 16.dp,
-                    top = 16.dp,
-                    end = 16.dp,
-                ).align(Alignment.End),
-            onClick = { isExpanded = !isExpanded },
-        ) {
-            Icon(
-                painter = painterResource(if (isExpanded) R.drawable.collapse else R.drawable.expand),
-                contentDescription = null,
-            )
-
-            Spacer(Modifier.width(ButtonDefaults.IconSpacing))
-
-            Text(if (isExpanded) "縮小" else "展開")
-        }
-
-        if (isExpanded) {
-            Text(
-                text = text,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp),
-            )
-        } else {
-            Text(
-                text = text,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp),
-                maxLines = 3,
-            )
-        }
     }
 }
 
